9dda6773b8bc117d8e3cc9ade38e580a397cfe3c
m-manju <manju.m@coddletech.com>
2023-10-31 11:30:19 +0530

    updated

--- login-page/src/components/footer.tsx
+++ login-page/src/components/footer.tsx
@@ -4,50 +4,12 @@ import './../App.css';
 const Footer: React.FC = () => {
   return (
     <>
-    <footer>
-        <div className="container footercls">
-          <div className="footer-text">
-            <h3>EpicEntertain</h3>
-            <h5>Empowering Knowledge, One Page at a Time 
-                Empowering Knowledge, One Page at a Time 
-                Empowering Knowledge, One Page at a Time!!</h5>
-          </div>
-          <div className="footerul">
-            <h4 className="footer-h4">About</h4>
-            <ul className="ul">
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Features</a></li>
-                <li><a href="#">Contact us</a></li>
-            </ul>
-          </div>
-          <div className="footerul">
-            <h4 className="footer-h4">Company</h4>
-            <ul className="ul">
-                <li><a href="#">Our office</a></li>
-                <li><a href="#">Touch with us</a></li>
-            </ul>
-          </div>
-          <div className="footerul">
-            <h4 className="footer-h4">Support</h4>
-            <ul className="ul">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Support carrier</a></li>
-                <li><a href="#">Contact us</a></li>
-            </ul>
-          </div>
-          <div className="footerul">
-            <h4 className="footer-h4">Get in touch</h4>
-            <ul className="ul">
-                <li><a href="#">About us About us</a></li>
-                <li><a href="#">Features About us about</a></li>
-            </ul>
-            <div className="footerIcons">
-              <a href="#" className="fab fa-instagram"></a>
-              <a href="#" className="fab fa-facebook"></a>
-              <a href="#" className="fab fa-twitter"></a>
-            </div>
-          </div>
-        </div>
+    <footer className="footer">
+      
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Your Company</p>
+        <p>Privacy Policy | Terms of Service</p>
+      </div>
     </footer>
     </>
   );
